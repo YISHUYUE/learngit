668d6332892fc5367039e6166535f4e683c4944e
Yishuyue <847010402@qq.com>
2016-10-13 19:56:30 +0800

    wrote a yishuyue file.

---
+++ check.js
@@ -0,0 +1,20 @@
+function onblus1(){
+    if(names.value=="")
+        { names.placeholder="名字在哪里"}
+}
+function onblus2(){
+    if(numm.value=="")
+        { numm.placeholder="嘿，你的学号呢"}
+}
+function onblus3(){
+    if(types.value=="")
+        { types.placeholder="说说专业呗"}
+}
+function onblus4(){
+    if(phones.value=="")
+        {phones.placeholder="联系方式"}
+}
+function onblus5(){
+    if(ins.value=="")
+        {ins.placeholder="请在此处介绍自己..."}
+}
